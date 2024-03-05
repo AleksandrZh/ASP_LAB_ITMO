c9e74514e5bd3ce9e0216aac87063cb63a1d2b38
ZhmakinAA <ZhmakinAA@hotmail.ru>
2024-03-05 15:30:50 +0300

    Add project files.

---
+++ StudentRecord_control/ITMO.ASP.StudentRecords.DAL/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using ITMO.ASP.StudentRecords.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.ASP.StudentRecords.DAL
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Score> Scores { get; set; }
+
+        public ApplicationContext()
+            : base("StudentDB")
+        { 
+           
+        }
+    }
+}
